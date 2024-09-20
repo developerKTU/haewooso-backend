0aa697d9686d350d352c4f69356aada387132a55
dnrdldnrl <rlaxodnr457@naver.com>
2024-09-20 15:28:51 +0900

    FCM SDK Deploy

--- src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
+++ src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
@@ -4,45 +4,48 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
-import java.util.*;
+import java.util.List;
 
 @Configuration
 public class FCMConfig {
 
-//    @Bean
-//    FirebaseMessaging firebaseMessaging() throws IOException {
-//        // Local
-//        //ClassPathResource resource = new ClassPathResource("firebase/haewooso-firebase-adminsdk-r1plg-b5fdea6348.json");
-//
-//        // 운영
-//        ClassPathResource resource = new ClassPathResource("/app/build/classes/java/main/com/ktu/haewooso_ver2/firebase/haewooso-firebase-adminsdk-r1plg-3e9c10ed08.json");
-//
-//        InputStream refreshToken = resource.getInputStream();
-//
-//        FirebaseApp firebaseApp = null;
-//        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
-//
-//        if(firebaseAppList != null && !firebaseAppList.isEmpty()){
-//            for(FirebaseApp app : firebaseAppList){
-//                if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)){
-//                    firebaseApp = app;
-//                }
-//            }
-//        }else{
-//            FirebaseOptions options = FirebaseOptions.builder()
-//                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
-//                    .build();
-//
-//            firebaseApp = FirebaseApp.initializeApp(options);
-//        }
-//
-//        return FirebaseMessaging.getInstance(firebaseApp);
-//    }
+    // 운영
+    @Value("${FCMSDK}")
+    static String FCMSDK;
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        // Local
+        //ClassPathResource resource = new ClassPathResource("firebase/haewooso-firebase-adminsdk-r1plg-b5fdea6348.json");
+        //InputStream refreshToken = resource.getInputStream();
+
+        // 운영
+        InputStream refreshToken = new ByteArrayInputStream(FCMSDK.getBytes());
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if(firebaseAppList != null && !firebaseAppList.isEmpty()){
+            for(FirebaseApp app : firebaseAppList){
+                if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)){
+                    firebaseApp = app;
+                }
+            }
+        }else{
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
 }
