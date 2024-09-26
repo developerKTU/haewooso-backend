95868f69e00534f229149802b36991cb680a4cea
dnrdldnrl <rlaxodnr457@naver.com>
2024-09-26 18:23:03 +0900

    git ignore add

---
+++ src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig_dev.java
@@ -0,0 +1,51 @@
+package com.ktu.haewooso_ver2.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+@Component
+public class FCMConfig_dev {
+
+    // 운영
+    @Value("${FCMSDK}")
+    private String FCMSDK;
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        // Local
+        ClassPathResource resource = new ClassPathResource("firebase/haewooso-firebase-adminsdk-r1plg-b5fdea6348.json");
+        InputStream refreshToken = resource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if(firebaseAppList != null && !firebaseAppList.isEmpty()){
+            for(FirebaseApp app : firebaseAppList){
+                if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)){
+                    firebaseApp = app;
+                }
+            }
+        }else{
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
