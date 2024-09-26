b0eeec10458adf967e43c4a7919e9c21b1453406
dnrdldnrl <rlaxodnr457@naver.com>
2024-09-26 21:18:17 +0900

    FCM Config 운영서버용으로 푸시

--- src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
+++ src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
@@ -4,22 +4,32 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 @Configuration
+@Component
 public class FCMConfig {
+
+    // 운영
+    @Value("${FCMSDK}")
+    private String FCMSDK;
 
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         // Local
-        ClassPathResource resource = new ClassPathResource("firebase/haewooso-firebase-adminsdk-r1plg-b5fdea6348.json");
-        InputStream refreshToken = resource.getInputStream();
+        //ClassPathResource resource = new ClassPathResource("firebase/haewooso-firebase-adminsdk-r1plg-b5fdea6348.json");
+        //InputStream refreshToken = resource.getInputStream();
+
+        // 운영
+        InputStream refreshToken = new ByteArrayInputStream(FCMSDK.getBytes());
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
