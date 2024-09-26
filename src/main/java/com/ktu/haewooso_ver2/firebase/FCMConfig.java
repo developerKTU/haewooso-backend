1e3282b997fafe0730627bfed252b826220a78c5
dnrdldnrl <rlaxodnr457@naver.com>
2024-09-26 21:06:28 +0900

    Member Entity -> useYn column add

--- src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
+++ src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
