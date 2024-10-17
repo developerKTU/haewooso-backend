db12fe1651e1d104735353da07d0bc4a1f715882
dnrdldnrl <rlaxodnr457@naver.com>
2024-10-17 11:25:10 +0900

    [fix] : 받은 메시지 내역 조회 시 페이징 값도 같이 리턴

--- src/main/java/com/ktu/haewooso_ver2/repository/jpaRepository/MessageBoardJpaRepository.java
+++ src/main/java/com/ktu/haewooso_ver2/repository/jpaRepository/MessageBoardJpaRepository.java
@@ -2,6 +2,8 @@ package com.ktu.haewooso_ver2.repository.jpaRepository;
 
 import com.ktu.haewooso_ver2.domain.pushMessage.SendMsg;
 import com.ktu.haewooso_ver2.dto.board.MessageBoardDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +15,19 @@ public interface MessageBoardJpaRepository extends JpaRepository<SendMsg, Long> 
     @Query("select s.sendUuid from SendMsg s where s.sendUuid not in (:myUUID) and s.receiveUuid = :myUUID group by s.sendUuid")
     List<String> getSendMeUuidList(@Param("myUUID") String myUUID);
 
-    @Query(value = "select title as title, content as content, secret_at as secretAt from haewooso.send_msg where send_uuid not in (:myUUID) and receive_uuid = :myUUID order by secret_at"
+    @Query(value = "select send_board_sno as id" +
+                   "     , title as title" +
+                   "     , content as content" +
+                   "     , secret_at as secretAt" +
+                   "  from haewooso.send_msg" +
+                   " where send_uuid not in (:myUUID)" +
+                   "   and receive_uuid = :myUUID" +
+                   " order by secret_at"
+            , countQuery = "select count(*)" +
+                           "  from haewooso.send_msg" +
+                           " where send_uuid not in (:myUUID)" +
+                           "   and receive_uuid = :myUUID"
             , nativeQuery = true)
-    List<MessageBoardDto> getMyBoardList(@Param("myUUID") String myUUID);
+    Page<MessageBoardDto> getMyBoardList(@Param("myUUID") String myUUID, Pageable pageable);
 
 }
