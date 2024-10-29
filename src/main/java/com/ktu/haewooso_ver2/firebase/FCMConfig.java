35438bef2f3aa716efc96af5147764296cb4e100
dnrdldnrl <rlaxodnr457@naver.com>
2024-10-29 15:06:53 +0900

    [fix] : FCMConfig.java 운영과 개발 코드 통일

--- src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
+++ src/main/java/com/ktu/haewooso_ver2/firebase/FCMConfig.java
@@ -7,7 +7,6 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
